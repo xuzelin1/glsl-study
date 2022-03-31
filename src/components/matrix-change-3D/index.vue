6f3d7f01bb5c54f7201473d45cac6216f7b485e6
zelin.xu <zelin.xu@shopee.com>
2022-03-31 22:05:12 +0800

    feat: 3d matrix

---
+++ src/components/matrix-change-3D/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <canvas id="matrix-canvas-3d"></canvas>
+
+    <div id="uiContainer">
+      <div id="ui">
+        <div id="x"></div>
+        <div id="y"></div>
+        <div id="z"></div>
+        <div id="angleX"></div>
+        <div id="angleY"></div>
+        <div id="angleZ"></div>
+        <div id="scaleX"></div>
+        <div id="scaleY"></div>
+        <div id="scaleZ"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import { main } from './index';
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      main();
+    });
+  },
+})
+</script>
+
+<style scoped>
+  @import url("https://webgl2fundamentals.org/webgl/resources/webgl-tutorials.css");
+  #matrix-canvas {
+    width: 100vw;
+    height: 100vh;
+    border: 2px solid #000;
+    margin: auto;
+  }
+  #uiContainer {
+    position: absolute;
+    top: 20px;
+    right: 50px;
+  }
+</style>
--- src/components/matrix-change/index.ts
+++ src/components/matrix-change/index.ts
@@ -172,9 +172,11 @@ export const main = () => {
     // Set the translation.
     gl.uniform2fv(translationLocation, translation);
 
+    // console.log(rotationLocation, rotation);
     // Set the rotation.
     gl.uniform2fv(rotationLocation, rotation);
 
+    console.log(scaleLocation, scale);
     // Set the scale.
     gl.uniform2fv(scaleLocation, scale);
 
@@ -192,29 +194,29 @@ function setGeometry(gl) {
   gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([
-        // left column
-        0, 0,
-        30, 0,
-        0, 150,
-        0, 150,
-        30, 0,
-        30, 150,
+      // left column
+      0, 0,
+      30, 0,
+      0, 150,
+      0, 150,
+      30, 0,
+      30, 150,
 
-        // top rung
-        30, 0,
-        100, 0,
-        30, 30,
-        30, 30,
-        100, 0,
-        100, 30,
+      // top rung
+      30, 0,
+      100, 0,
+      30, 30,
+      30, 30,
+      100, 0,
+      100, 30,
 
-        // middle rung
-        30, 60,
-        67, 60,
-        30, 90,
-        30, 90,
-        67, 60,
-        67, 90,
+      // middle rung
+      30, 60,
+      67, 60,
+      30, 90,
+      30, 90,
+      67, 60,
+      67, 90,
     ]),
     gl.STATIC_DRAW
   );
